9c8a665a6daf2dae78b445ecdcaa91479e0f496e
t-naganuma <takuya.naganuma@gmail.com>
2021-09-10 22:31:02 +0900

    サムネイル追加

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,10 +1,20 @@
 import { client } from '../../libs/client';
 import styles from '../../styles/Home.module.scss';
+import Image from 'next/image';
 
 export default function BlogId({ blog }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
+      <figure className={styles.thumbnail}>
+        <Image
+          src={blog.thumbnail.url}
+          alt={blog.title}
+          width={960}
+          height={540}
+          layout="responsive"
+        />
+      </figure>
       <p className={styles.publishedAt}>{blog.publishedAt}</p>
       <p className="category">{blog.category && `${blog.category.name}`}</p>
       <div
